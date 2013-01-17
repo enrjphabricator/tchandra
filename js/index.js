b0e2226de44f5811d90e2c3b0c88ca879fe17959
Jérôme Lepeltier <jerome.lepeltier@gmail.com>
2013-01-17 22:50:24 +0100

    fix css

--- js/index.js
+++ js/index.js
@@ -1,29 +1,40 @@
-if ( Modernizr.csstransforms ) {
-    var ul = document.getElementById('nav-slider').children[0];
-    var length  = ul.childElementCount;
-    var mySwipe = new Swipe(document.getElementById('slider'), {
-        auto : 4000,
-        callback: function(event, index, element){
-            ul.children[(index - 1 + length) % length].className = '';
-            ul.children[index].className = 'hover';
-        }
-    });
-    ul.children[0].className = 'hover';
-    document.getElementById('slider').onmouseleave  = function()
-    {
-        mySwipe.resume();
-    };
-
-    for(var i = 0; i < length; i++)
-    {
-        (function(i)
+window.onload=function(){
+    if ( Modernizr.csstransforms ) {
+        var ul = document.getElementById('nav-slider').children[0];
+        var length  = ul.childElementCount;
+        var mySwipe = new Swipe(document.getElementById('slider'), {
+            auto : 4000,
+            callback: function(event, index, element){
+                ul.children[(index - 1 + length) % length].className = '';
+                if (mySwipe.getPos() == index)
+                {
+                    ul.children[index].className = 'hover';
+                }
+            }
+        });
+        ul.children[0].className = 'hover';
+        document.getElementById('slider').onmouseleave  = function()
         {
-            ul.children[i].onmouseover = function()
+           if (!mySwipe.delay)
+           {
+                mySwipe.resume();
+           }
+        };
+        document.getElementById('slider').resize = function(e)
+        {
+            e.stopPropagation();
+        };
+        for(var i = 0; i < length; i++)
+        {
+            (function(i)
             {
-                mySwipe.stop();
-                ul.children[mySwipe.getPos()].className = '';
-                mySwipe.slide(i);
-            };
-        })(i);
+                ul.children[i].onmouseover = function()
+                {
+                    mySwipe.stop();
+                    ul.children[mySwipe.getPos()].className = '';
+                    mySwipe.slide(i);
+                };
+            })(i);
+        }
     }
 }
